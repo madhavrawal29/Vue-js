56f63dfa73ee3ad071cf4d0594c0d60a4ebb1df9
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-06-21 23:59:00 +0530

    Interpolation and Data, functions and variables

--- src/components/MyFirstComponent.vue
+++ src/components/MyFirstComponent.vue
@@ -1,8 +1,28 @@
-<script></script>
-
 <template>
-  <h1>My First Component</h1>
+  <h1>My First Component {{ 10 + 10 }}</h1>
+  <h1>My email {{ email }}</h1>
+  <h1>My Name {{ name }}</h1>
+  <h2>My Changed Name {{ (name = 'Maddy') }}</h2>
+  <h2>Function inside function and my age {{ age() }}</h2>
+  <h2>My height Function with Parameter {{ height('5,4') }}</h2>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: 'mad@gmail.com',
+      name: 'Madhav Rawal',
+      age: function () {
+        return 24
+      },
+      height: function (height) {
+        return height
+      }
+    }
+  }
+}
+</script>
 
 <style scoped>
 h1 {
